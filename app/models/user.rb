d3bb9856f13adaf8b5905e5523b77f927654f1e7
James Romanchuk <romanchukenator@gmail.com>
2014-03-30 09:56:37 +0000

    Adding validations for creations of guesses and user accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
-# class User < ActiveRecord::Base
-#   has_many :player1_games, class_name: "Game", foreign_key: 'player1_id'
-#   has_many :player2_games, class_name: "Game", foreign_key: 'player2_id'
-# end
-
 class User < ActiveRecord::Base
   has_many :games
+
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :email, length:{ minimum: 5 }
+
+  validates :password, presence: true
+  validates :password, length: { minimum: 4 }
+
 end
